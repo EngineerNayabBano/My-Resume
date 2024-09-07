9064b14039fc947d1adfeef71fcaa4a5538e5e2b
EngineerNayabBano <nayab.bano2000@gmail.com>
2024-09-08 02:18:00 +0500

    My resume task hackathon
    using html css javascript and typescript

---
+++ my resume/script.js
@@ -0,0 +1,8 @@
+var nameElement = document.getElementById("name");
+var contactElement = document.getElementById("contact");
+var user = {
+    name: "Nayab Bano",
+    contact: "nayab.bano2000@gamil.com | (123) 423-7330"
+};
+nameElement.textContent = user.name;
+contactElement.textContent = user.contact;
---
+++ my resume/script.ts
@@ -0,0 +1,9 @@
+const nameElement = document.getElementById("name");
+const contactElement = document.getElementById("contact");
+
+const userName = {
+    name: "Nayab Bano",
+    contact: "nayab.bano2000@gamil.com | (123) 423-7330"};
+
+    nameElement! .textContent = user.name;
+    contactElement!.textContent = user.contact;
